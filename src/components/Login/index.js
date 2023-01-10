4752f5c4d776d8241d1f0e8788ad79204207739e
Anjana19JK <anjanajoseph199@gmail.com>
2023-01-10 23:15:33 +0530

    Update index.js

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,46 +1,48 @@
-import {Component} from 'react'
 import Cookies from 'js-cookie'
-import {Redirect} from 'react-router-dom'
+import {Redirect, withRouter} from 'react-router-dom'
 
 import './index.css'
 
-class Login extends Component {
-  onSubmitSuccess = jwtToken => {
-    Cookies.set('jwt_token', jwtToken, {
+const Login = props => {
+  const jwtToken = Cookies.get('jwt_token')
+
+  const setCookiesAndNavigateToHome = token => {
+    const {history} = props
+
+    Cookies.set('jwt_token', token, {
       expires: 30,
-      path: '/',
     })
-    const {history} = this.props
     history.replace('/')
   }
 
-  submitForm = async event => {
-    event.preventDefault()
+  const onClickLogin = async () => {
+    const userDetails = {username: 'rahul', password: 'rahul@2021'}
     const url = 'https://apis.ccbp.in/login'
     const options = {
       method: 'POST',
+      body: JSON.stringify(userDetails),
     }
     const response = await fetch(url, options)
     const data = await response.json()
+
     if (response.ok === true) {
-      this.onSubmitSuccess(data.jwt_token)
+      setCookiesAndNavigateToHome(data.jwt_token)
     }
   }
 
-  render() {
-    const jwtToken = Cookies.get('jwt_token')
-    if (jwtToken !== undefined) {
-      return <Redirect to="/" />
-    }
-    return (
-      <div>
-        <h1>Please Login</h1>
-        <button type="button" onSubmit={this.submitForm}>
-          Login with Sample Creds
-        </button>
-      </div>
-    )
+  if (jwtToken !== undefined) {
+    return <Redirect to="/" />
   }
+
+  return (
+    <div className="login">
+      <h1>Please Login</h1>
+      <button type="button" onClick={onClickLogin}>
+        Login with Sample Creds
+      </button>
+    </div>
+  )
 }
 
-export default Login
+export default withRouter(Login)
+
